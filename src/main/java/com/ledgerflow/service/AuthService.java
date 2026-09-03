3da540248ff8d0cc38333350dcd8769af98c95ba
Heito~ <heitorbr963@gmail.com>
2026-09-03 20:18:07 -0300

    [Login] Service Melhorado/SHA256 implementado

--- src/main/java/com/ledgerflow/service/AuthService.java
+++ src/main/java/com/ledgerflow/service/AuthService.java
@@ -1,27 +1,31 @@
 package com.ledgerflow.service;
 
-import com.ledgerflow.model.Sessao;
+import com.ledgerflow.model.PopupWarning;
 import com.ledgerflow.model.Usuario;
-import org.mindrot.jbcrypt.BCrypt;
 import java.sql.SQLException;
+import static org.apache.commons.codec.digest.DigestUtils.*;
 
 public class AuthService {
 
-    private final UsuarioService UserService;
+    private final UsuarioService userService;
     private Usuario userAtual;
 
     public AuthService(UsuarioService UserService) {
-        this.UserService = UserService;
+        this.userService = UserService;
     }
 
     public boolean Login(String email, String senha) throws SQLException {
-        if (email.isEmpty() || senha.isEmpty()) return false;
-        var senhaHash = BCrypt.hashpw(senha, BCrypt.gensalt());
+        if (email.isBlank() || senha.isBlank()) return false;
 
-        var user = UserService.findByEmail(email);
-        if (user == null) return false;
+        var senhaHash = sha256Hex(senha);
 
-        if(user.getSenha().equals(senhaHash) && user.getEmail().equals(email)) {
+        var user = userService.findByEmail(email);
+        if (user == null) {
+            PopupWarning.warning("Sem cadastro","Email não encontrado");
+            return false;
+        }
+
+        if(user.getSenha().equals(senhaHash)){
             userAtual = user;
             return true;
         }
